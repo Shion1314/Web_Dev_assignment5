20bd42f339ad5709e1819cb840754c3efcffaef8
Shion1314 <jiaxinlin200204@gmail.com>
2023-11-20 11:34:47 -0500

    applying route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import * as React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import * as ReactDOM from "react-dom/client";
+import Home from "./Home"
 import "./styles.css";
 
-import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "Web_Dev_assignment5/",
+    element: <Home/>,
+  }
+  ]
+);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
